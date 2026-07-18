731187a3d4af9f511eaaa06931fed0d9991f5ed0
amitdevx <iaminfinity.gamer123@gmail.com>
2026-07-18 11:37:07 +0530

    chore: organize scratch test files and configure gitignore

--- scratch.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import { parseMarkdown } from './dist/parser/index.js';
-
-async function test() {
-  const md = fs.readFileSync('tests/fixtures/page-breaks.md', 'utf-8');
-  const res = await parseMarkdown(md, { pageBreaks: { h1NewPage: true, hrAsPageBreak: true } });
-  console.log(res.html);
-}
-test();
