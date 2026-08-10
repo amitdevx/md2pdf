a34dab5f1fb889c9e57e7fcb1c996e2f598bf69c
amitdevx <iaminfinity.gamer123@gmail.com>
2026-08-10 19:50:44 +0530

    chore: remove garbage files

--- test-browser.js
+++
@@ -1,6 +0,0 @@
-import { getBrowser } from './dist/pdf/browser.js';
-console.log("Starting getBrowser...");
-const b = await getBrowser();
-console.log("Got browser");
-await b.close();
-console.log("Closed browser");
