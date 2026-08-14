38ad4369cf90e71dae76e5f9a8a5eab21a33b051
amitdevx <iaminfinity.gamer123@gmail.com>
2026-08-14 21:08:55 +0530

    test: increase vitest testTimeout to 120s to fix CI timeouts

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     include: ['tests/**/*.test.ts'],
-    testTimeout: 60000,
+    testTimeout: 120000,
     pool: 'forks',
     poolOptions: {
       forks: {
